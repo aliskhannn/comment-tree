970bd69a410538ac6bfda84a24be315074420f56
Aliskhan <aliskhanreventon@gmail.com>
2025-09-19 10:57:21 +0300

    feat: add comment model

---
+++ backend/internal/model/comment.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Comment struct {
+	ID        uuid.UUID `json:"id"`
+	ParentID  uuid.UUID `json:"parent_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
